c4efaeced9ac8c31edb590280284e63cbb257df6
ChiomaOnyebuchi <110993371+Favy429@users.noreply.github.com>
2026-05-24 22:27:37 +0100

    Add files via upload

---
+++ profile.py
@@ -0,0 +1,19 @@
+print("Hello World")
+print("My name is Favour")
+print("I am becoming a Python Developer")
+print("I currently work as a supervisor/ leader of a sales team in an FMCG company")
+print("I studied Chemical Engineering, but I have zero experience in the field.") 
+print("However, I have 7 years experience in the supervisory role")
+target_salary = "$100/hr"
+print("My target salary is:", target_salary)
+temperature = 3.14
+name = 'Favour'
+
+result = temperature + len(name)
+print("OUTPUT:", result)
+
+# Creates a class named 'MyClass' inheriting from 'object' with an attribute 'x'
+MyClass = type('MyClass', (object,), {'x': 100})
+obj = MyClass()
+print(obj.x)  
+
